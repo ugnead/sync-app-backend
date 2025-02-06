86e637de7964a62addb7feabf2734856246e5bf7
Ugne Adomaityte <u.adomaityte98@gmail.com>
2025-02-06 23:10:18 +0200

    Add AuthController for Google authentication routes

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return {
+      message: 'User info from Google',
+      user: req.user,
+    };
+  }
+}
